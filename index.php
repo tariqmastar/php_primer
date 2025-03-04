0e980a27e502925f912d18fbf97ac60e3d78c162
tariqmastar <tariq1310@gmail.com>
2025-03-04 12:33:51 +0000

    add three more buttons

--- index.php
+++ index.php
@@ -29,6 +29,9 @@
 
     ?>
     <button type="button" class="btn btn-dark">CLICK ME!</button>
+    <button type="button" class="btn btn-primary">CLICK ME!</button>
+    <button type="button" class="btn btn-success">CLICK ME!</button>
+    <button type="button" class="btn btn-info">CLICK ME!</button>
 
     <?php 
         require 'includes/footer.php'
